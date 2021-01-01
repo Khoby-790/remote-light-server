3a38206de14bbbe395fe520f02c59c0e8d590265
Khoby-790 <ebaidoo79@gmail.com>
2021-01-01 18:07:31 +0800

    Server Setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").ls
+const port = 3000;
+
+
+io.on("connection", socket => {
+    console.log("a user connected :D");
+    socket.on("chat message", msg => {
+        console.log(msg);
+        io.emit("chat message", msg);
+    });
+});
+
+server.listen(port, () => console.log("server running on port:" + port));
